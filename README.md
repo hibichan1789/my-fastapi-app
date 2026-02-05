ffd6c2104f38d72983031b87fa817548d5a73f10
hibiki <takechi.hibiki1234@gmail.com>
2026-02-05 21:09:16 +0900

    郵便番号検索のベースの実装

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # python fastAPI練習  
+郵便番号を打つことで住所の取得が可能  
+数字7桁のみ受け付けるようにバリデーションを実装
+# 外部APIの利用について
+本プロジェクトでは以下の外部APIを利用しています  
+- [zipcode](https://zipcloud.ibsnet.co.jp/)
+- [zipcodeAPI利用規約](https://zipcloud.ibsnet.co.jp/rule/api)
 
 
 # 環境変数の準備
@@ -12,3 +18,11 @@ Dockerfile,docker-compose.yml,.envとかを編集したらbuild
 # 所感
 動作確認用のswaggerを立ち上げるのに苦労した  
 IPv6とIPv4の競合？でlocalhostでできないことを何とかするのに苦労した
+
+# 今日学んだこと
+dockerの環境構築名前空間の衝突  
+ctrl + @, ctrl + 1これ便利すぎる  
+ni ファイル名  
+cp コピー元 コピー先  
+.env ファイルの参考として.env.sampleを作成しておく,env.sampleにはAPIキーは直接書かないあくまで例だけ  
+rm 削除したいファイル名
--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException
 import os
-app = FastAPI()
+import httpx
 
+app = FastAPI()
 @app.get("/")
 def get_root():
     test_value = os.environ.get("TEST_VALUE")
     return{"messages": "動作確認", "test_value": test_value}
+
+@app.get("/weather/{zipcode}")
+async def get_current_weather(zipcode:str=Path(..., pattern=r"^\d{7}$")):
+    zipcode_url = os.environ.get("ZIPCODEURL")
+    if not zipcode_url:
+        raise HTTPException(status_code=500, detail="Server error")
+    try:
+        async with httpx.AsyncClient() as client:
+            zipcode_response = await client.get(zipcode_url, params={"zipcode": zipcode})
+            zipcode_response.raise_for_status()
+    except httpx.HTTPStatusError as e:
+        raise HTTPException(status_code=502)
+    zipcode_json = zipcode_response.json()
+    return zipcode_json
